7fe4029157096d8168553db5829d60d0119e4b90
Anuj Gore <anujgore23@gmail.com>
2025-03-08 13:43:06 +0000

    First commit.

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll(".nav-button");
+  const title = document.querySelector('h1'); // Assuming your title is an <h1> tag
+
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      const page = this.getAttribute('data-page'); // Get the data-page attribute value
+      if (page) {
+        window.location.href = `webpages/${page}.html`; // Construct the URL
+      } else {
+        console.error("Button missing data-page attribute");
+      }
+    });
+  });
+
+
+
+  if (title) {
+    title.addEventListener('click', function() {
+      window.location.href = '../index.html'; // Navigate back to index.html
+    });
+  }
+});
