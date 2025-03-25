db6fb7dacd08a097071f4011fdb1f3645d6ab5f1
AnujGore <anujgore23@gmail.com>
2025-03-25 23:11:11 +0000

    finished book site

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const books = [
-    { title: "Born a Crime", cover: "assets/born_a_crime_cover.jpg"},
-    { title: "Butter", cover: "assets/butter_cover.jpg"},
-    { title: "Five Decembers", cover: "assets/five_decembers.jpg"},
-    { title: "Kafka on the Shore", cover: "assets/kafka_on_the_shore.jpg"},
-    { title: "Mysterious Affair at Styles", cover: "assets/mysterious_affair_at_styles.jpg"}
+    { cover: "assets/born_a_crime_cover.jpg", rating: 4},
+    { cover: "assets/butter_cover.jpg", rating: 4.5},
+    { cover: "assets/five_decembers.jpg", rating: 3.5},
+    { cover: "assets/kafka_on_the_shore.jpg", rating: 3.5},
+    { cover: "assets/mysterious_affair_at_styles.jpg", rating: 4},
+    { cover: "assets/orbital.jpg", rating: 2.5},
+    { cover: "assets/the_anxious_generation.jpg", rating: 3.5},
+    { cover: "assets/the_sellout.jpg", rating: 4.5},
+    { cover: "assets/you_are_here.jpg", rating: 3},
+    { cover: "assets/so_much_blue.jpg", rating: 3.5},
+    { cover: "assets/a_man_with_one_of_those_faces.jpg", rating: 4},
+    { cover: "assets/the_memory_police.jpg", rating: 2},
+    { cover: "assets/death_on_the_nile.jpg", rating: 4.5},
+    { cover: "assets/the_white_tiger.jpg", rating: 3},
+    { cover: "assets/the_windfall.jpg", rating: 4},
+    { cover: "assets/the_ride_of_a_lifetime.jpg", rating: 4},
+    { cover: "assets/the_satsuma_complex.jpg", rating: 4.5},
+    { cover: "assets/a_promised_land.jpg", rating: 4.5},
+    { cover: "assets/catcher_in_the_rye.jpg", rating: 3},
+    { cover: "assets/animal-farm.jpg", rating: 4.5},
+    { cover: "assets/1984.jpg", rating: 4.5},
+    { cover: "assets/to_kill_a_mockingbird.jpg", rating: 3.5}
 ];
 
 
@@ -43,10 +60,29 @@ function loadBookstoPage(books, html_ID) {
     books.forEach((book) => {
         let div = document.createElement("div");
         div.classList.add("book");
-        div.innerHTML = `<img src="${book.cover}" alt="${book.title}" />`;
+        div.innerHTML = `
+        <img src="${book.cover}" alt="${book.title}" />
+        <p class = "rating-container">${displayRating(book.rating)}</p>`;
+
         html_ID.appendChild(div);
-        
+
     });
 
     return html_ID;
 }
+
+function displayRating(rating) {
+    let stars = '';
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0; // Check if there's a decimal part
+
+    for (let i = 0; i < fullStars; i++) {
+      stars += '&#9733;';
+    }
+  
+    if (hasHalfStar) {
+      stars += '&#11240;'; // Example: HTML entity for fraction one-half
+    }
+  
+    return `<p>${stars}</p>`;
+  }
